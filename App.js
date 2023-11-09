f2a3ef1dd31814a368e9645ea55f8a68e01e1487
Leonardo <leoruiz.carr@gmail.com>
2023-11-09 11:50:49 -0300

    Commit projeto

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import Task from './Task';
+import styles from './styles.js';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>agora vai o commit!</Text>
-      <StatusBar style="auto" />
+      {/* Scroll para ver a lista de tarefas */}
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        keyboardShouldPersistTaps='handled'
+      >
+
+        {/* Tarefas de hoje */}
+
+        {/* CABEÇALHO BRANCO */}
+
+          {/* <View style={{ width: '100%', height: 80, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center' }}>
+          </View> */}
+
+        {/* fim */}
+
+        {/* AQUI COMEÇA DO TEXTO "TAREFA" PARA BAIXO */}
+        <View style={styles.tasksWrapper}>
+          <Text style={styles.sectionTitle}>Tarefas</Text>
+          <View style={styles.items}>
+
+            {/*Aqui entra a TASK*/}{
+              taskItems.map((item, index) => {
+                return (
+                  <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                    <Task text={item} />
+                  </TouchableOpacity>
+                )
+              })
+            }
+          </View>
+        </View>
+
+      </ScrollView>
+
+      {/* Escrever tarefa */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Escreva uma tarefa'} value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
