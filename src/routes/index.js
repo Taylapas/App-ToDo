d331e98b8bf7d2cbf3775d15431d18775b06ce0d
Taylapas <94627835+Taylapas@users.noreply.github.com>
2023-11-20 22:50:10 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import Welcome from '../Pages/Welcome'
 import SignIn from '../Pages/SignIn'
 import Tasks from '../Pages/Tasks'
-import { Header } from '@react-navigation/stack';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -11,16 +11,24 @@ export default function Routes(){
     return(
     <Stack.Navigator>
         <Stack.Screen
-        nome = "Welcome"  
+        name = "Welcome"  
         component={Welcome}
         options={{headerShown: false}}
         />
 
         <Stack.Screen
-        nome = "SignIn"  
+        name = "SignIn"  
         component={SignIn}
         options={{headerShown: false}}
         />
-    /</Stack.Navigator>
+
+        <Stack.Screen
+        name = "Tasks"
+        component={Tasks}
+        options={{headerShown:false}}
+        />
+
+        
+    </Stack.Navigator>
     )
     }
